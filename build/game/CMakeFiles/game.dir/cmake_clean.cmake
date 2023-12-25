1851f50dbe5272e9d4e817ba0275f6d0e4020997
RichardCzar <64083265+Ricsi1992@users.noreply.github.com>
2023-12-26 00:32:35 +0100

    implement cmake

---
+++ build/game/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "../bin/game.exe"
+  "../bin/game.exe.manifest"
+  "../bin/game.pdb"
+  "CMakeFiles/game.dir/src/ASCIIRenderEngine.cpp.obj"
+  "CMakeFiles/game.dir/src/ASCIIRenderEngine.cpp.obj.d"
+  "CMakeFiles/game.dir/src/GameEngine.cpp.obj"
+  "CMakeFiles/game.dir/src/GameEngine.cpp.obj.d"
+  "CMakeFiles/game.dir/src/GameStateHandler.cpp.obj"
+  "CMakeFiles/game.dir/src/GameStateHandler.cpp.obj.d"
+  "CMakeFiles/game.dir/src/main.cpp.obj"
+  "CMakeFiles/game.dir/src/main.cpp.obj.d"
+  "libgame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
